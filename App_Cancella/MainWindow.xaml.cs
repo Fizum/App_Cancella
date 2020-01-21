c2776f5895afb5fd3156042e0d470462b5b75c84
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2020-01-21 09:54:32 +0100

    Stop dell0'esecuzione di un esecuzione versione 2

--- App_Cancella/MainWindow.xaml.cs
+++ App_Cancella/MainWindow.xaml.cs
@@ -21,45 +21,56 @@ namespace App_Cancella
     /// </summary>
     public partial class MainWindow : Window
     {
-        bool stop = false;
+        //bool stop = false;
         public MainWindow()
         {
             InitializeComponent();
         }
 
+        CancellationTokenSource ct;
+
         private void Btn_Start1_Click(object sender, RoutedEventArgs e)
         {
-            if (stop)
-                stop = false;
-            Task.Factory.StartNew(() => DoWork(100, 1000, lbl_Count1));
+            if (ct == null)
+                ct = new CancellationTokenSource();
+            //if (stop)
+            //    stop = false;
+            Task.Factory.StartNew(() => DoWork(100, 1000, lbl_Count1, ct));
         }
 
         private void Btn_Start2_Click(object sender, RoutedEventArgs e)
         {
-            if (stop)
-                stop = false;
+            if (ct == null)
+                ct = new CancellationTokenSource();
+            //if (stop)
+            //    stop = false;
             int max = Convert.ToInt32(txt_Max1.Text);
-            Task.Factory.StartNew(() => DoWork(max, 1000, lbl_Count2));
+            Task.Factory.StartNew(() => DoWork(max, 1000, lbl_Count2, ct));
         }
 
         private void Btn_Start3_Click(object sender, RoutedEventArgs e)
         {
-            if (stop)
-                stop = false;
+            if (ct == null)
+                ct = new CancellationTokenSource();
+            //if (stop)
+            //    stop = false;
             int max = Convert.ToInt32(txt_Max2.Text);
             int delay = Convert.ToInt32(txt_Delay.Text);
-            Task.Factory.StartNew(() => DoWork(max, delay, lbl_Count3));
+            Task.Factory.StartNew(() => DoWork(max, delay, lbl_Count3, ct));
         }
 
-        private void DoWork(int max, int delay, Label lbl)
+        private void DoWork(int max, int delay, Label lbl, CancellationTokenSource ct)
         {
             for (int i = 0; i <= max; i++)
             {
-                Dispatcher.Invoke(()=> UpdateUI(i, lbl));
+                Dispatcher.Invoke(() => UpdateUI(i, lbl));
                 Thread.Sleep(delay);
 
-                if (stop)
-                    break;
+                if (ct.Token.IsCancellationRequested)
+                    break; //gracefully
+
+                //if (stop)
+                //    break;
             }
         }
 
@@ -70,7 +81,12 @@ namespace App_Cancella
 
         private void Btn_Stop_Click(object sender, RoutedEventArgs e)
         {
-            stop = true;
+            if (ct != null)
+            {
+                ct.Cancel();
+                ct = null;
+            }
+
         }
     }
 }
